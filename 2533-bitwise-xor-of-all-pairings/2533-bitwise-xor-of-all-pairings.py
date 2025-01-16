2818df1212f18cb3b563fc5cfb493000ea8afffd
Vishwes G <vicchugobu@gmail.com>
2025-01-16 23:57:55 +0530

    Time: 0 ms (100%), Space: 36.6 MB (48.56%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.py
@@ -1,14 +1,11 @@
 class Solution:
-    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        len1, len2 = len(nums1), len(nums2)
-        freq = {}
-        for num in nums1:
-            freq[num] = freq.get(num, 0) + len2
-        for num in nums2:
-            freq[num] = freq.get(num, 0) + len1
-        ans = 0
-        for num in freq:
-            if freq[num] % 2:
-                ans ^= num
-
-        return ans
+    def xorAllNums(self, nums1, nums2):
+        n, m = len(nums1), len(nums2)
+        counter = 0
+        if n % 2 == 1:
+            for num in nums2:
+                counter ^= num
+        if m % 2 == 1:
+            for num in nums1:
+                counter ^= num
+        return counter
