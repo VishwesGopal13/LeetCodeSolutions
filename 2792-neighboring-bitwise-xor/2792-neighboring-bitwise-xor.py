2a4b0e1b05f9e9ea5c3e5b02785f83dc00cb7ace
Vishwes G <vicchugobu@gmail.com>
2025-01-17 22:53:02 +0530

    Time: 43 ms (67.15%), Space: 22.5 MB (7.3%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
@@ -1,4 +1,13 @@
-class Solution:
-    def doesValidArrayExist(self, derived: List[int]) -> bool:
-        return reduce(xor, derived)==0
+class Solution(object):
+    def doesValidArrayExist(self, derived):
+        """
+        :type derived: List[int]
+        :rtype: bool
+        """
+        n = len(derived)
+        xor_sum = 0        
+        for num in derived:
+            xor_sum ^= num
+        
+        return xor_sum == 0
         
