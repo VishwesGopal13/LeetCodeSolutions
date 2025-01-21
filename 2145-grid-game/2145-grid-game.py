fd29ab05df6c2cad436f393b28663bb9347837c9
Vishwes G <vicchugobu@gmail.com>
2025-01-21 20:47:04 +0530

    Time: 91 ms (69.51%), Space: 29.6 MB (62.95%) - LeetHub

--- 2145-grid-game/2145-grid-game.py
+++ 2145-grid-game/2145-grid-game.py
@@ -1,12 +1,15 @@
 class Solution:
-    def gridGame(self, grid):
-        top = sum(grid[0])
-        bottom = 0
-        res = float('inf')
-        
-        for i in range(len(grid[0])):
-            top -= grid[0][i]
-            res = min(res, max(top, bottom))
-            bottom += grid[1][i]
-        
-        return res
+    def gridGame(self, grid: List[List[int]]) -> int:
+        n=len(grid[0]) 
+        U=sum(grid[0])-grid[0][0] 
+        D=0
+        Robot2=U 
+
+        for i in range(1, n): 
+            U-=grid[0][i] 
+            D+=grid[1][i-1]
+
+            P=max(U, D)
+            Robot2=min(Robot2, P)
+
+        return Robot2
