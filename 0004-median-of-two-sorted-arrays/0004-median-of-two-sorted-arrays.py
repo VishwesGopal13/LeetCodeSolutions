a5636604b7a3ebb5adabe362fdf5491d9feb94e7
Vishwes G <vicchugobu@gmail.com>
2025-08-15 23:10:25 +0530

    Time: 2 ms (56.98%), Space: 18 MB (58.41%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,24 +1,26 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A,B=nums1,nums2
-        n=len(A)+len(B)
-        h=n//2
         if len(B)<len(A):
             A,B=B,A
+        n=len(A)+len(B)
+        h=n//2
+
         l,r=0,len(A)-1
         while True:
             i=(l+r)//2
             j=h-i-2
-            Al=A[i] if i>=0 else float('-inf')
-            Ar=A[i+1] if i+1<len(A) else float('inf')
-            Bl=B[j] if j>=0 else float('-inf')
-            Br=B[j+1] if j+1<len(B) else float('inf')
 
-            if Al<=Br and Bl<=Ar:
+            AL=A[i] if i>=0 else float('-inf')
+            AR=A[i+1] if (i+1)<len(A) else float('inf')
+            BL=B[j] if j>=0 else float('-inf')
+            BR=B[j+1] if (j+1)<len(B) else float('inf')
+
+            if AL<=BR and BL<=AR:
                 if n%2==1:
-                    return min(Ar,Br)
-                return (max(Al,Bl)+min(Ar,Br))/2
-            elif Al>Br:
+                    return min(AR,BR)
+                return (max(AL,BL)+min(AR,BR))/2
+            elif AL>BR:
                 r=i-1
             else:
                 l=i+1
